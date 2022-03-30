84bef7a1fb674d388b415cf11312dde2e0adf599
Preetika <mishraashish1989@gmail.com>
2022-03-30 14:36:25 +0530

    first commit

---
+++ src/test/java/framework/SeleniumTest.java
@@ -0,0 +1,18 @@
+package framework;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test	
+	public void SelTest1()
+	{
+		System.out.println("SeleniumTest1");
+	}
+	@Test
+	public void SelTest2()
+	{
+		System.out.println("SeleniumTest2");
+	}
+	
+
+}
